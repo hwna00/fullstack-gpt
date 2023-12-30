4795009dd2f74ecb856ed541c4e965196f921229
hwna00 <layton3534@gmail.com>
2023-12-30 18:58:15 +0900

    <Feat> Question Prompts

--- pages/QuizGPT.py
+++ pages/QuizGPT.py
@@ -2,7 +2,9 @@ import streamlit as st
 from langchain.retrievers import WikipediaRetriever
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
-
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
 
 st.set_page_config(
     page_title="Quiz GPT",
@@ -10,6 +12,13 @@ st.set_page_config(
 )
 
 st.title("Quiz GPT")
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    model="gpt-3.5-turbo-1106",
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
+)
 
 
 @st.cache_data(show_spinner="Loading file...")
@@ -31,7 +40,12 @@ def split_file(file):
     return docs
 
 
+def format_docs(docs):
+    return "\n\n".join(docuement.page_content for docuement in docs)
+
+
 with st.sidebar:
+    docs = None
     choice = st.selectbox(
         "Choose what you want to use.",
         ("File", "Wikipedia Article"),
@@ -52,3 +66,57 @@ with st.sidebar:
             retriever = WikipediaRetriever(top_k_results=2)
             with st.status("Search Wikipedia.."):
                 docs = retriever.get_relevant_documents(topic)
+
+if not docs:
+    st.markdown(
+        """
+        Welcome to Quiz GPT
+
+        I will make a quiz from Wikipedia articles or
+        files you upload to test your knowledge and help you study.
+
+        Get started by uploading a file or searching on Wikipeida in the sidebar.
+        """
+    )
+else:
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+                You are a helpful assistant that is role playing as a teacher.
+                    
+                Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+                
+                Each question should have 4 answers, three of them must be incorrect and one should be correct.
+                    
+                Use (o) to signal the correct answer.
+                    
+                Question examples:
+                    
+                Question: What is the color of the ocean?
+                Answers: Red|Yellow|Green|Blue(o)
+                    
+                Question: What is the capital or Georgia?
+                Answers: Baku|Tbilisi(o)|Manila|Beirut
+                    
+                Question: When was Avatar released?
+                Answers: 2007|2001|2009(o)|1998
+                    
+                Question: Who was Julius Caesar?
+                Answers: A Roman Emperor(o)|Painter|Actor|Model
+                    
+                Your turn!
+                    
+                Context: {context}
+                """,
+            )
+        ]
+    )
+
+    chain = {"context": format_docs} | prompt | llm
+
+    start = st.button("Generate Quiz")
+
+    if start:
+        chain.invoke(docs)
