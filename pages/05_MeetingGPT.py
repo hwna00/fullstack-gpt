2ecdfbaa4362f10628c5e19014ebf6d5da800869
hwna00 <layton3534@gmail.com>
2024-01-08 14:46:30 +0900

    <Feat> Refine Chain

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -5,6 +5,15 @@ from pydub import AudioSegment
 import glob
 import openai
 import os
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import StrOutputParser
+
+llm = ChatOpenAI(
+    temperature=0.1,
+)
 
 st.set_page_config(page_title="Meeting GPT", page_icon="💼")
 st.title("Meeting GPT")
@@ -64,7 +73,7 @@ with st.sidebar:
 
 if video:
     chunks_path = "./.cache/chunks"
-    with st.status("Loading video..."):
+    with st.status("Loading video...") as status:
         video_content = video.read()
         video_path = f".cache/{video.name}"
         audio_path = video_path.replace("mp4", "mp3")
@@ -72,11 +81,68 @@ if video:
         with open(video_path, "wb") as f:
             f.write(video_content)
 
-    with st.status("Extracting audio..."):
-        extract_audio_from_video(video_path)
+    status.update(label="Extracting audio...")
+    extract_audio_from_video(video_path)
 
-    with st.status("Cutting audio segments..."):
-        cut_audio_in_chunks(audio_path, 6, chunks_path)
+    status.update(label="Cutting audio segments...")
+    cut_audio_in_chunks(audio_path, 6, chunks_path)
 
-    with st.status("Transcribe audio..."):
-        transcribe_chunks(chunks_path, transcript_path)
+    status.update(label="Transcribing audio...")
+    transcribe_chunks(chunks_path, transcript_path)
+
+    transcript_tab, summary_tab, qa_tab = st.tabs(["Transcript", "Summary", "Q&A"])
+
+    with transcript_tab:
+        with open(transcript_path, "r") as file:
+            st.write(file.read())
+
+    with summary_tab:
+        start = st.button("Generate summary")
+
+        if start:
+            loader = TextLoader(transcript_path)
+            splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+                chunk_size=800,
+                chunk_overlap=100,
+            )
+            docs = loader.load_and_split(text_splitter=splitter)
+
+            first_summary_prompt = ChatPromptTemplate.from_template(
+                """
+                Write a concise summary of the following:
+                "{text}"
+                CONCISE SUMMARY:
+                """
+            )
+
+            first_summary_chain = first_summary_prompt | llm | StrOutputParser()
+            summary = first_summary_chain.invoke({"text": docs[0].page_content})
+
+            refine_prompt = ChatPromptTemplate.from_template(
+                """
+                Your job is to provide a final summary.
+                We have provided an existing summary up to a certain point: {existing_summary}
+                We have the opportunity to refine the existing summary (only if needed) with some more context below.
+                ----------
+                {context}
+                ----------
+                Given the new context, refine the original summary.
+                If the context isn't useful, RETURN the original summary.
+                """
+            )
+
+            refine_chain = refine_prompt | llm | StrOutputParser()
+
+            with st.status("Summarizing...") as status:
+                for idx, doc in enumerate(docs[1:]):
+                    status.update(label=f"Processing document {idx+1}/{len(docs)-1}")
+                    summary = refine_chain.invoke(
+                        {
+                            "existing_summary": summary,
+                            "context": doc.page_content,
+                        }
+                    )
+            summary
+
+    with qa_tab:
+        pass
